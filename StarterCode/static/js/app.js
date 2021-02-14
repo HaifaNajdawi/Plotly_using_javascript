96e4c67ee91b54d98c3762d1ad6942821fbc6b55
Haifa <haifanaj@gmail.com>
2021-02-14 17:15:20 -0600

    create option list

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,11 +1,33 @@
-json_path="../../../data/samples.json"
+json_path = "../../../data/samples.json"
 
 
-d3.json(json_path).then(function(data) {
-    console.log(data);
-  });
-  
-  // Promise Pending
-  const dataPromise = d3.json(json_path);
-  console.log("Data Promise: ", dataPromise);
-  
+d3.json(json_path).then(function (data) {
+    console.log("data", data);
+});
+
+// Promise Pending
+const dataPromise = d3.json(json_path);
+console.log("Data Promise: ", dataPromise);
+
+// Returns an array of values
+function unpack(rows, index) {
+    return rows.map(function (row) {
+        return row[index];
+    });
+}
+listId = d3.selectAll("#selDataset")
+
+listId.on("click", function getData() {
+    let optionList = document.getElementById('selDataset').options;
+
+    d3.json(json_path).then(function (data) {
+        sample = data.samples
+        console.log("samples", sample)
+        sample.forEach(value => optionList.add(new Option(value.id)) )
+
+        
+
+    })
+
+})
+
