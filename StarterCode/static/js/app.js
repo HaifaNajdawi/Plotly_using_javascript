7f0d2bcc141c2cc4b22c64e3b3ad03219011b18f
Haifa <haifanaj@gmail.com>
2021-02-14 21:59:50 -0600

    comment the code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,8 +2,11 @@ json_path = "../../../data/samples.json"
 
 function init() {
     data = [{
+        // values
         x: [5, 6, 7, 8],
+        // labels
         y: ["A", "B", "C", "D"],
+        // texthover
         text: ['A', 'B', 'C', 'D'],
         type: "bar",
         orientation: "h",
@@ -16,7 +19,7 @@ function init() {
     }
 
     Plotly.newPlot("bar", data,layout);
-}
+};
 
 
 
@@ -28,49 +31,56 @@ d3.json(json_path).then(function (data) {
 const dataPromise = d3.json(json_path);
 console.log("Data Promise: ", dataPromise);
 
+// select id that will work on it 
 listId = d3.selectAll("#selDataset")
 
 listId.on("click", function getData() {
+    // option list to push values to it 
     let optionList = document.getElementById('selDataset').options;
 
     d3.json(json_path).then(function (data) {
+        // samples key in json
         sample = data.samples
-        id = sample.forEach(value => optionList.add(new Option(value.id)));
 
+        // loop through and add the id to option 
+        sample.forEach(value => optionList.add(new Option(value.id)));
 
     });
 
 });
 
 d3.selectAll("body").on("change", updatePage);
-
+// bar chart data 
 function updatePage() {
     // Use D3 to select the dropdown menu
     var dropdownMenu = d3.selectAll("#selDataset").node();
     // Assign the dropdown menu option to a variable
     var selectedOption = dropdownMenu.value;
     console.log(selectedOption);
-    otuIdsFilter = sample.filter(row => row.id === selectedOption)[0]
+    // filter data from the user 
+    otuIdsFilter = sample.filter(row => row.id === selectedOption)[0];
 
+    // label chart and convert it to string 
     arrayOtu = otuIdsFilter.otu_ids;
-    y = arrayOtu.slice(0, 10).map(yy=>yy.toString()+" OTU").reverse()
+    y = arrayOtu.slice(0, 10).map(yy=>"OTU "+ yy.toString()+"  ").reverse();
 
+    // values in x asis 
     arraySample = otuIdsFilter.sample_values;
-    x = arraySample.slice(0, 10).reverse()
+    x = arraySample.slice(0, 10).reverse();
 
-    arrayText = otuIdsFilter.otu_labels
-    text = arrayText.slice(0, 10).reverse()
+    // texthover 
+    arrayText = otuIdsFilter.otu_labels;
+    text = arrayText.slice(0, 10).reverse();
 
+    // update the plot data 
     Plotly.restyle("bar", "x", [x]);
     Plotly.restyle("bar", "y", [y]);
     Plotly.restyle("bar", "text", [text]);
 
     console.log("y",y)
 
-
-
-}
-init()
+};
+init();
 
 
 
